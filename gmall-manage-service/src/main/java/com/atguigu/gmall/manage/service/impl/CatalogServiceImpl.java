8abfb1070531c9dddf7db5790644afdff034380c
wangjinglong <wang09512@126.com>
2019-12-25 13:40:43 +0800

    二 三级分类完善

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/CatalogServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/CatalogServiceImpl.java
@@ -2,7 +2,11 @@ package com.atguigu.gmall.manage.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.atguigu.gmall.bean.PmsBaseCatalog1;
+import com.atguigu.gmall.bean.PmsBaseCatalog2;
+import com.atguigu.gmall.bean.PmsBaseCatalog3;
 import com.atguigu.gmall.manage.mapper.CatalogMapper;
+import com.atguigu.gmall.manage.mapper.PmsCatalog2Mapper;
+import com.atguigu.gmall.manage.mapper.PmsCatalog3Mapper;
 import com.atguigu.gmall.service.CatalogService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,8 +22,32 @@ public class CatalogServiceImpl  implements CatalogService {
     @Autowired
     CatalogMapper catalogMapper;
 
+    @Autowired
+    PmsCatalog2Mapper pmsCatalog2Mapper;
+
+    @Autowired
+    PmsCatalog3Mapper pmsCatalog3Mapper;
+
     @Override
     public List<PmsBaseCatalog1> getCatalog1() {
         return catalogMapper.selectAll();
     }
+
+    @Override
+    public List<PmsBaseCatalog2> getCatalog2(String catalog1Id) {
+        PmsBaseCatalog2 catalog2 = new PmsBaseCatalog2();
+        catalog2.setCatalog1Id(catalog1Id);
+        List<PmsBaseCatalog2> catalog2s = pmsCatalog2Mapper.select(catalog2);
+        return catalog2s;
+    }
+
+    @Override
+    public List<PmsBaseCatalog3> getCatalog3(String catalog2Id) {
+        PmsBaseCatalog3 catalog3 = new PmsBaseCatalog3();
+        catalog3.setCatalog2Id(catalog2Id);
+        List<PmsBaseCatalog3> catalog3s = pmsCatalog3Mapper.select(catalog3);
+        return catalog3s;
+    }
+
+
 }
--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/CatalogController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/CatalogController.java
@@ -2,6 +2,8 @@ package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsBaseCatalog1;
+import com.atguigu.gmall.bean.PmsBaseCatalog2;
+import com.atguigu.gmall.bean.PmsBaseCatalog3;
 import com.atguigu.gmall.service.CatalogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -33,4 +35,18 @@ public class CatalogController {
         List<PmsBaseCatalog1> pmsBaseCatalog1s =  catalogService.getCatalog1();
         return pmsBaseCatalog1s;
     }
+
+
+    @RequestMapping("getCatalog2")
+    public List<PmsBaseCatalog2> getCatalog2(String catalog1Id){
+        List<PmsBaseCatalog2> pmsBaseCatalog2s =  catalogService.getCatalog2(catalog1Id);
+        return pmsBaseCatalog2s;
+    }
+
+    @RequestMapping("getCatalog3")
+    public List<PmsBaseCatalog3> getCatalog3(String catalog2Id){
+        List<PmsBaseCatalog3> pmsBaseCatalog3s =  catalogService.getCatalog3(catalog2Id);
+        return pmsBaseCatalog3s;
+    }
+
 }
